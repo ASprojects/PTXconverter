3d936e4937b829fb166d7c904fe849cd7393c6d5
ASprojects <adam.stradomski0@gmail.com>
2021-04-24 18:41:15 +0200

    clmn names

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ all_files = glob.glob(path + "/*.PTX")
 
 li = []
 
+column_names = []
+for i in range(31):
+    x = "column " + str(i)
+    column_names.append(x)
+
 for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=None, skiprows=3, sep=',', error_bad_lines=False, names=list(range(31)))
+    df = pd.read_csv(filename, index_col=None, header=None, skiprows=3, sep=',', error_bad_lines=False, names=list(column_names))
     li.append(df)
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
 df.to_csv(r'G:/Test.csv', index=False)
+
