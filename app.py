60d57a7eacb8c2dded4dae53297c10e699b5ba8e
ASprojects <adam.stradomski0@gmail.com>
2021-04-24 18:19:38 +0200

    loading multpiple datas

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import glob
 
-df = pd.read_csv('G:/000067_001_EG_PB_H3331_ST10_18.ptx',
-                 header=None, skiprows=3, sep=',', error_bad_lines=False, names=list(range(31)))
+path = r'G:\Convert'
+all_files = glob.glob(path + "/*.PTX")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=None, skiprows=3, sep=',', error_bad_lines=False, names=list(range(31)))
+    li.append(df)
+
+df = pd.concat(li, axis=0, ignore_index=True)
 
 df.to_csv(r'G:/Test.csv', index=False)
-
