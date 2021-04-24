0a218b74c10af14ccf727503eac0b5c231edfee6
ASprojects <adam.stradomski0@gmail.com>
2021-04-24 20:22:41 +0200

    loaded full data

--- app.py
+++ app.py
@@ -12,10 +12,33 @@ for i in range(31):
     column_names.append(x)
 
 for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=None, skiprows=3, sep=',', error_bad_lines=False, names=list(column_names))
+    df = pd.read_csv(filename, index_col=None, header=None, skiprows=3, sep=',',
+                     error_bad_lines=False, names=list(column_names))
     li.append(df)
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
-df.to_csv(r'G:/Test.csv', index=False)
+df_req = (df.loc[df['column 0'] == 'PARTS_REQ'])
+df_inf = (df.loc[df['column 0'] == 'PARTS_INF'])
 
+
+# column_names_2 = []
+# for i in range(31):
+#     x = "column x" + str(i)
+#     column_names_2.append(x)
+# df_inf.columns = list(column_names_2)
+
+
+df_inf = df_inf.reset_index(drop=True)
+df_req = df_req.reset_index(drop=True)
+
+
+if len(df_inf) == len(df_req):
+    df_summary = pd.concat([df_req, df_inf], axis=1, ignore_index=True).to_csv(r'G:/df_summary.csv', index=True)
+
+
+# if len(df_inf) == len(df_req):
+#     df_summary = pd.merge(df_req, df_inf, left_index=True, right_index=True).to_csv(r'G:/df_summary.csv', index=True)
+
+# if len(df_inf) == len(df_req):
+#     df_summary = df_req.join(df_inf).to_csv(r'G:/df_summary.csv', index=True)
