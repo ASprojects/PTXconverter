a66f9d329c0e2aaff61c6cdeb3b072ed8517d5c6
ASprojects <adam.stradomski0@gmail.com>
2021-04-25 13:45:21 +0200

    encoded

--- app.py
+++ app.py
@@ -18,6 +18,22 @@ for filename in all_files:
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
+
+# for column in df.columns:
+#             df[column] = df[column].astype("str")
+#                                    .str.normalize('NFKD')
+#                                    .str.encode('ascii', errors='ignore')
+#                                    .str.decode('utf-8')
+
+
+df['column 3'] = (df['column 3'].astype("str")
+                        .str.normalize('NFKD')
+                        .str.encode('ascii', errors='ignore')
+                        .str.decode('utf-8')
+                        .str.replace("ciana", "Sciana")
+                        .str.replace("grny", "gorny")
+                        .str.replace("pka", "polka"))
+
 df_req = (df.loc[df['column 0'] == 'PARTS_REQ'])
 df_inf = (df.loc[df['column 0'] == 'PARTS_INF'])
 
@@ -32,17 +48,35 @@ df_inf = (df.loc[df['column 0'] == 'PARTS_INF'])
 df_inf = df_inf.reset_index(drop=True)
 df_req = df_req.reset_index(drop=True)
 
-unnecesary_columns = [0, 1, 4, 61]
-for i in range(7, 33):
+unnecesary_columns = [0, 1, 4, 35, 36, 37, 48, 61]
+for i in range(7, 34):
     unnecesary_columns.append(i)
 for i in range(43, 47):
     unnecesary_columns.append(i)
 for i in range(51, 59):
     unnecesary_columns.append(i)
 
+column_names2 = ['linia w ptx',
+                 'Nazwa',
+                 'Dlugosc',
+                 'Szerokosc',
+                 'Typ',
+                 'Numer Zlecenia',
+                 'Okleina 1',
+                 'Okleina 2',
+                 'Okleina 3',
+                 'Okleina 4',
+                 'Nazwa Zlecenia',
+                 'Material',
+                 'LP',
+                 'CNC1',
+                 'CNC2']
+
 if len(df_inf) == len(df_req):
     df_summary = pd.concat([df_req, df_inf], axis=1, ignore_index=True).drop(columns=unnecesary_columns)
-    df_summary.sort_values(by=3).reset_index(drop=True).to_excel(r'G:/df_summary.xlsx', index=True)
+    df_final = df_summary.reset_index(drop=True).sort_values(by=50).reset_index(drop=True)
+    df_final.columns = column_names2
+    df_final.to_excel(r'G:/df_summary.xlsx', index=True)
 else:
     print("error")
 
